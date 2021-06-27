f24b9dd5d8fb5c2e37bf808f84df142c26541da3
fz6m <fz6meng@gmail.com>
2021-06-28 01:20:45 +0800

    fix: example talk text tooltip and update avatar 🍰

--- src/pages/Home/CenterContent/AutoTalkCom/index.tsx
+++ src/pages/Home/CenterContent/AutoTalkCom/index.tsx
@@ -39,40 +39,39 @@ export const AutoTalkCom: FC = () => {
         const talkElm = (
           <>
             {avatarElm}
-            {textElm}
+            <Tooltip
+              placement={isEven ? 'top' : 'bottom'}
+              arrowPointAtCenter
+              title={item.desc}
+              zIndex={1}
+              overlayClassName={styles.j_tooltip}
+            >
+              {textElm}
+            </Tooltip>
           </>
         )
 
         return (
-          <Tooltip
-            placement={isEven ? 'top' : 'bottom'}
-            arrowPointAtCenter
-            title={item.desc}
-            zIndex={1}
-            key={index}
-            overlayClassName={styles.j_tooltip}
+          <motion.div
+            className={styles.talk}
+            style={{
+              flexFlow: isEven ? 'row-reverse' : undefined,
+            }}
+            initial={{
+              opacity: 0,
+              x: isEven ? 20 : -20,
+            }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: {
+                delay: index * 0.5 + 0.7,
+                type: 'spring',
+              },
+            }}
           >
-            <motion.div
-              className={styles.talk}
-              style={{
-                flexFlow: isEven ? 'row-reverse' : undefined,
-              }}
-              initial={{
-                opacity: 0,
-                x: isEven ? 20 : -20,
-              }}
-              animate={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  delay: index * 0.5 + 0.7,
-                  type: 'spring',
-                },
-              }}
-            >
-              {talkElm}
-            </motion.div>
-          </Tooltip>
+            {talkElm}
+          </motion.div>
         )
       })}
     </div>
