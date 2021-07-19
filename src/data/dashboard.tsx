77c14298ffb8945063ff88cabb8c17dd5d8d57be
fz6m <fz6meng@gmail.com>
2021-07-20 03:18:22 +0800

    chore: update dashboard data 🍥

--- src/data/dashboard.tsx
+++ src/data/dashboard.tsx
@@ -4,12 +4,12 @@ export const DASHBOARD_CONFIG: Pick<IData, 'dashboard'> = {
   dashboard: [
     // 1 line
     {
-      value: 1347,
+      value: 1413,
       unit: '枚',
       desc: '累计服务',
     },
     {
-      value: '9.49',
+      value: '10.54',
       unit: '亿',
       desc: '累计收包',
     },
@@ -20,7 +20,7 @@ export const DASHBOARD_CONFIG: Pick<IData, 'dashboard'> = {
     },
     // 2 line
     {
-      value: 886,
+      value: 950,
       unit: '个',
       desc: '公益服务',
     },
