3f86c56bb16ba69874966c86194265c966e7d138
fz6m <59400654+fz6m@users.noreply.github.com>
2022-01-02 04:26:18 +0800

    feat: fix header height not spread and add header room 🍇

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export const REVEAL_CLASS = 'j-scroll-reveal_'
+
+export const HEADER_PIN_CLASS = 'j-header-pin'
--- src/pages/Home/LogoHeader/index.tsx
+++ src/pages/Home/LogoHeader/index.tsx
@@ -4,50 +4,56 @@ import { Menu, Tooltip } from 'antd'
 import { GithubOutlined } from '@ant-design/icons'
 import { text } from '@data/index'
 import cx from 'classnames'
-import { REVEAL_CLASS } from '@constants/index'
+import { HEADER_PIN_CLASS, REVEAL_CLASS } from '@constants/index'
+import Headroom from 'react-headroom'
 
 export const LogoHeader: FC = () => {
-  // 备用
-  // const onClickLogo = useCallback(() => {
-  //   window.scrollTo({
-  //     left: 0,
-  //     top: 0,
-  //     behavior: 'smooth',
-  //   })
-  // }, [])
+  const onClickLogo = useCallback(() => {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: 'smooth',
+    })
+  }, [])
 
   return (
-    <div className={cx(styles.header, REVEAL_CLASS)}>
-      <div className={styles.logo}>{text.header.title}</div>
-      <div className={styles.header_right}>
-        <Menu mode="horizontal" className={styles.menu} selectable={false}>
-          {text.header.menu.map((item, index) => (
-            <Menu.Item
-              key={index}
-              className={styles.menu_item}
-              icon={item.icon}
-            >
-              {item.link ? (
-                <a href={item.link} target="_blank" rel="noreferrer">
-                  {item.name}
-                </a>
-              ) : (
-                item.name
-              )}
-            </Menu.Item>
-          ))}
-        </Menu>
-        <Tooltip title="项目地址" placement="bottomRight">
-          <a
-            className={styles.github_btn}
-            href={text.header.github}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GithubOutlined className={styles.github_icon} />
-          </a>
-        </Tooltip>
+    <Headroom className={styles.header_pin}>
+      <div className={cx(styles.header, REVEAL_CLASS, HEADER_PIN_CLASS)}>
+        <div className={styles.logo} onClick={onClickLogo}>
+          {text.header.title}
+        </div>
+        <div className={styles.header_right}>
+          <Menu mode="horizontal" className={styles.menu} selectable={false}>
+            {text.header.menu.map((item, index) => (
+              <Menu.Item
+                key={index}
+                className={styles.menu_item}
+                icon={item.icon}
+              >
+                {item.link ? (
+                  <a href={item.link} target="_blank" rel="noreferrer">
+                    {item.name}
+                  </a>
+                ) : (
+                  item.name
+                )}
+              </Menu.Item>
+            ))}
+          </Menu>
+          <div className={styles.github_wrapper}>
+            <Tooltip title="项目地址" placement="bottomRight">
+              <a
+                className={styles.github_btn}
+                href={text.header.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubOutlined className={styles.github_icon} />
+              </a>
+            </Tooltip>
+          </div>
+        </div>
       </div>
-    </div>
+    </Headroom>
   )
 }
